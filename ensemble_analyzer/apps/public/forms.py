5b11293ad8094ca2e3c559ed4b07f3f1e23fb2ee
afbly <afableafrahly@gmail.com>
2022-03-01 23:07:17 +0800

    fix validations

--- ensemble_analyzer/apps/public/forms.py
+++ ensemble_analyzer/apps/public/forms.py
@@ -50,18 +50,18 @@ class InputForm(forms.Form):
         cleaned_data = super(InputForm, self).clean()
         text = cleaned_data.get('text')
         csv = cleaned_data.get('csv')
+        
         self.isCSV = False 
         self.isText = False
+        self.isNotLarge = False
+        self.isNotInput = False
         self.inputText = text
 
         if csv:
-            print("hehe")
-            self.isCSV = True
-            print(csv.name) #prints the file name in the console
-
             # file type validation
             ext = os.path.splitext(csv.name)[1] # returns path+filename
             valid_filetype = ['.csv']
+            
             if ext.lower() in valid_filetype:
                 try:
                     #read uploaded csv
@@ -72,28 +72,30 @@ class InputForm(forms.Form):
 
                 # the system has a note that the column the user wants to classify must be renamed as "input"
                 # convert "input column into list"
-                self.included_col = data['input'].tolist()
+                cols = [col for col in data.columns]
+                if 'input' in cols:
+                    self.included_col = data['input'].tolist()
+                    # check no. of rows
+                    self.check_rows = (len(self.included_col))
 
-                # check no. of rows
-                self.check_rows = (len(self.included_col))
-
-                # number of rows validation
-                if self.check_rows <= 499:
-                    # proceed to backend
-                    print("proceed to backend")
-
-
-                # else if row is greater than 500, raise csv_rowError validation then reload page
+                    # number of rows validation
+                    if self.check_rows <= 499:
+                        # proceed to backend
+                        self.isCSV = True
+                        print(csv.name) #prints the file name in the console
+                        print("proceed to backend")
+                    else:
+                        # else if row is greater than 500, raise csv_rowError validation then reload page
+                        self.isNotLarge = True
+                        raise forms.ValidationError('large message here')
                 else:
-                    raise forms.ValidationError('large message here')
-            
-            # else if filetype is not supported, raise filetype_error validation then reload page
+                    self.isNotInput = True
+                    raise forms.ValidationError('Columns no input')
             else:
+                # else if filetype is not supported, raise filetype_error validation then reload page
                 raise forms.ValidationError('filetypemessagehere')
-        
         elif text:
             self.isText = True
-
         else:
             raise forms.ValidationError('You have to write or upload something!')
 
@@ -101,4 +103,4 @@ class InputForm(forms.Form):
         return self.isText, self.inputText
 
     def get_csv(self):
-        self.included_col, self.check_rows, self.isCSV 
+        return self.included_col, self.check_rows, self.isCSV 
